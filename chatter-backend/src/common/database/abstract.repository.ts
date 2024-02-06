fd5d55e0d417b311a4f1869dc939d6f1930b567b
OkJa <nameofokja@gmail.com>
2024-02-06 19:55:27 +0900

    Naming Fixed :: Schema => Entity

--- chatter-backend/src/common/database/abstract.repository.ts
+++ chatter-backend/src/common/database/abstract.repository.ts
@@ -1,23 +1,23 @@
 import { Logger, NotFoundException } from '@nestjs/common';
-import { AbstractDocument } from './abstract.schema';
+import { AbstractEntity } from './abstract.entity';
 import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 
-export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+export abstract class AbstractRepository<T extends AbstractEntity> {
   protected abstract readonly logger: Logger;
 
-  constructor(protected readonly model: Model<TDocument>) {}
+  constructor(protected readonly model: Model<T>) {}
 
-  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+  async create(document: Omit<T, '_id'>): Promise<T> {
     const createDocumnet = new this.model({
       ...document,
       _id: new Types.ObjectId(),
     });
 
-    return (await createDocumnet.save()).toJSON() as unknown as TDocument;
+    return (await createDocumnet.save()).toJSON() as unknown as T;
   }
 
-  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
-    const document = await this.model.findOne(filterQuery).lean<TDocument>();
+  async findOne(filterQuery: FilterQuery<T>): Promise<T> {
+    const document = await this.model.findOne(filterQuery).lean<T>();
 
     if (!document) {
       this.logger.warn('Document was not found with filterQuery', filterQuery);
@@ -28,12 +28,12 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   }
 
   async findOneAndUpdate(
-    filterQuery: FilterQuery<TDocument>,
-    update: UpdateQuery<TDocument>,
-  ): Promise<TDocument> {
+    filterQuery: FilterQuery<T>,
+    update: UpdateQuery<T>,
+  ): Promise<T> {
     const document = await this.model
       .findOneAndUpdate(filterQuery, update, { new: true })
-      .lean<TDocument>();
+      .lean<T>();
 
     if (!document) {
       this.logger.warn('Document was not found with filterQuery', filterQuery);
@@ -43,13 +43,11 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     return document;
   }
 
-  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
-    return this.model.find(filterQuery).lean<TDocument[]>();
+  async find(filterQuery: FilterQuery<T>): Promise<T[]> {
+    return this.model.find(filterQuery).lean<T[]>();
   }
 
-  async findOneAndDelete(
-    filterQuery: FilterQuery<TDocument>,
-  ): Promise<TDocument> {
-    return this.model.findOneAndDelete(filterQuery).lean<TDocument>();
+  async findOneAndDelete(filterQuery: FilterQuery<T>): Promise<T> {
+    return this.model.findOneAndDelete(filterQuery).lean<T>();
   }
 }
--- chatter-backend/src/users/users.module.ts
+++ chatter-backend/src/users/users.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
+import { DatabaseModule } from 'src/common/database/database.module';
+import { User, UserSchema } from './entities/user.entity';
+import { UsersRepository } from './users.repository';
 
 @Module({
-  providers: [UsersResolver, UsersService],
+  imports: [
+    DatabaseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UsersResolver, UsersService, UsersRepository],
 })
 export class UsersModule {}
