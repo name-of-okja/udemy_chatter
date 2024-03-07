4ef88280770b5ecf5998fc2e76cad78619c5af7b
name-of-okja <nameofokja@gmail.com>
2024-03-07 16:35:49 +0900

    Docker Remove

--- chatter-backend/src/main.ts
+++ chatter-backend/src/main.ts
@@ -6,10 +6,13 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
   app.use(cookieParser());
+
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow('PORT'));
 }
